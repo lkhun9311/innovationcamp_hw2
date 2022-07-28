5ad5a028650728822e940d007d7cefe7f7d47b85
kwanghunlee <lkhun9311@naver.com>
2022-07-28 14:33:25 +0900

    Add Homework2Application 객체 생성, annotation 추가

--- homework2/src/main/java/com/sparta/homework2/Homework2Application.java
+++ homework2/src/main/java/com/sparta/homework2/Homework2Application.java
@@ -2,7 +2,9 @@ package com.sparta.homework2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Homework2Application {
 
